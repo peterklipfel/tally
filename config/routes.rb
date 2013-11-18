7da4ebbdb45102843ff9676cdd47b7c6012ca55e
Peter Klipfel <peter@klipfel.me>
2013-11-17 20:03:12 -0700

    scaffolded invoices

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Tally::Application.routes.draw do
+  resources :invoices
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
