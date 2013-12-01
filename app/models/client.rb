f07941b63be45bb69713962abf9ca337fc6c9747
Peter Klipfel <peter@klipfel.me>
2013-12-01 15:50:17 -0700

    hooked up models

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,5 @@
 #== Represents a client that will receive invoices
 class Client < ActiveRecord::Base
+  belongs_to :user
+  has_many :invoices
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,5 @@
 #== Represents a billable entitity that will be added to an invoice
 class Expense < ActiveRecord::Base
+  belongs_to :invoice
+  has_many :payments
 end
