dd35a6648767cb03d3def19e5bd563beb78b1ab4
Peter Klipfel <peter@klipfel.me>
2013-12-06 18:01:41 -0700

    ensuring that clients respond to related models

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe Client do
+  it { should respond_to :user}
+  it { should respond_to :invoices}
+
   it "fails validation with no user" do
     expect(Client.new).to have(1).error_on(:user)
   end
