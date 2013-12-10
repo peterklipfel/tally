5e18d82919074088ea4e8aa32fa7ae5c5c36e0c8
Peter Klipfel <peter@klipfel.me>
2013-12-09 21:24:24 -0700

    implemented hack for scope bug

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -4,7 +4,6 @@ class PaymentsController < ApplicationController
   # GET /payments
   # GET /payments.json
   def index
-    puts Expense.all_for_user(current_user).inspect
     @payments = Payment.all_for_user(current_user)
   end
 
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -4,5 +4,9 @@ class Payment < ActiveRecord::Base
   validates :expense, :amount, presence: true
   validates :amount, numericality: true
 
-  scope :all_for_user, -> (user_id) { Payment.joins(:expense).load.merge Expense.all_for_user(user_id) }
+  # The following version of the scope does not work currently because there is a bug in
+  # rails core.  I have requested that an issue be reopened at https://github.com/rails/rails/issues/3002
+  # scope :all_for_user, -> (user_id) { Payment.joins(:expense).load.merge Expense.all_for_user(user_id) }
+
+  scope :all_for_user, -> (user_id) { Expense.all_for_user(user_id).collect { |e| e.payments }.to_a.flatten }
 end
