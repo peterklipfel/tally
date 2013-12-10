51ed33f94ef56d8de14eabd25a661edd68c41db5
Peter Klipfel <peter@klipfel.me>
2013-12-09 22:05:54 -0700

    ensuring vaidations on payments

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Payment do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should respond_to :expense }
+
+  it "fails validation with no expense" do
+    expect(Payment.new).to have(1).error_on(:expense)
+  end
+  it "fails validation with no amount" do
+    expect(Payment.new).to have(2).error_on(:amount)
+  end
+  it "fails validation with non-numeric amount" do
+    expect(Payment.new(amount: "one hundred")).to have(1).error_on(:amount)
+  end
+
 end
