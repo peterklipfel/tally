9fab24cefd484a9c3d148512d84bbd7641c7f981
Devon Tivona <tivona@me.com>
2013-12-14 16:27:26 -0700

    Add support for the nested expenses in feature paths

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -1,16 +1,16 @@
 module NavigationHelper
-  def path_to(page_name, id=nil)
+  def path_to(page_name, item=nil)
     case page_name.downcase
     when /home/ then root_path
     when /sign in/ then new_user_session_path
     when /sign up/ then new_user_registration_path
     when /new invoice/ then new_invoice_path
-    when /show invoice/ then invoice_path(id)
-    when /edit invoice/ then edit_invoice_path(id)
+    when /show invoice/ then invoice_path(item)
+    when /edit invoice/ then edit_invoice_path(item)
     when /invoices/ then invoices_path
-    when /new expense/ then new_expense_path
-    when /edit expense/ then edit_expense_path(id)
-    when /edit client/ then edit_client_path(id)
+    when /new expense/ then new_invoice_expense_path(item)
+    when /edit expense/ then edit_invoice_expense_path(item.invoice, item)
+    when /edit client/ then edit_client_path(item)
     when /new client/ then new_client_path
     when /clients/ then clients_path
     else
