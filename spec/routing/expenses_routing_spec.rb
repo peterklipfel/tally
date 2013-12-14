704d7ef02919ed1917a38361b8343ca6e214f31d
Devon Tivona <tivona@me.com>
2013-12-14 16:31:25 -0700

    Add support for nesting in the expenses routing

--- spec/routing/expenses_routing_spec.rb
+++ spec/routing/expenses_routing_spec.rb
@@ -3,32 +3,28 @@ require "spec_helper"
 describe ExpensesController do
   describe "routing" do
 
-    it "routes to #index" do
-      get("/expenses").should route_to("expenses#index")
-    end
-
     it "routes to #new" do
-      get("/expenses/new").should route_to("expenses#new")
+      get("/invoices/1/expenses/new").should route_to("expenses#new", invoice_id: "1")
     end
 
     it "routes to #show" do
-      get("/expenses/1").should route_to("expenses#show", :id => "1")
+      get("/invoices/1/expenses/1").should route_to("expenses#show", id: "1", invoice_id: "1")
     end
 
     it "routes to #edit" do
-      get("/expenses/1/edit").should route_to("expenses#edit", :id => "1")
+      get("/invoices/1/expenses/1/edit").should route_to("expenses#edit", id: "1", invoice_id: "1")
     end
 
     it "routes to #create" do
-      post("/expenses").should route_to("expenses#create")
+      post("/invoices/1/expenses").should route_to("expenses#create", invoice_id: "1")
     end
 
     it "routes to #update" do
-      put("/expenses/1").should route_to("expenses#update", :id => "1")
+      put("/invoices/1/expenses/1").should route_to("expenses#update", id: "1", invoice_id: "1")
     end
 
     it "routes to #destroy" do
-      delete("/expenses/1").should route_to("expenses#destroy", :id => "1")
+      delete("/invoices/1/expenses/1").should route_to("expenses#destroy", id: "1", invoice_id: "1")
     end
 
   end
